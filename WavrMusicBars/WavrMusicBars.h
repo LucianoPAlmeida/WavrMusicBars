fe7e24770e88d7f1fcccc57f5193e97f2af7b4ec
LucianoAlmeida <passos.luciano@outlook.com>
2016-10-25 09:17:59 -0200

    Initial Commit

---
+++ WavrMusicBars/WavrMusicBars.h
@@ -0,0 +1,19 @@
+//
+//  WavrMusicBars.h
+//  WavrMusicBars
+//
+//  Created by Luciano Almeida on 10/25/16.
+//  Copyright © 2016 Luciano Almeida. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for WavrMusicBars.
+FOUNDATION_EXPORT double WavrMusicBarsVersionNumber;
+
+//! Project version string for WavrMusicBars.
+FOUNDATION_EXPORT const unsigned char WavrMusicBarsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <WavrMusicBars/PublicHeader.h>
+
+
